ccc394afd9a159f70a98c92cbeb688f9cb4c9fe0
oovaa <omarvx211@gmail.com>
2024-09-03 15:06:34 +0200

    chore: Add request logger and timer middleware

--- index.ts
+++ index.ts
@@ -1,17 +1,31 @@
-import express from 'express'
+import express, { type RequestHandler } from 'express'
 
 const app = express()
+app.use(express.json())
 
-const posts: any[] = []
+const posts: string[] = []
 
-posts.push('hi')
+const timer: RequestHandler = (req, res, next) => {
+  console.log(Date.now());
+  next()
+}
+const logger: RequestHandler = (req, res, next) => {
+  console.log("new request:", req.path, req.body);
+  next()
+}
+
+app.use(logger)
+app.use(timer)
+
 
 app.get('/posts', (req, res) => {
-  res.send(posts)
+  res.send({ posts })
 })
 
 app.post('/posts', (req, res) => {
-  console.log(req.body)
+  // console.log("body: ", req.body)
+  const newpost = req.body
+  posts.push(newpost)
   res.sendStatus(201)
 })
 
